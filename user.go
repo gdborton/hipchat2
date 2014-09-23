2e3d8e976852a74820cb4a31f19655d4a805822e
Gary Borton <gdborton@gmail.com>
2014-09-23 11:26:38 -0400

    Fix the tags on the user struct.

--- user.go
+++ user.go
@@ -11,43 +11,43 @@ type User struct {
 	// https://www.hipchat.com/docs/apiv2/method/view_user
 
 	// XMPP/Jabber ID of the user.
-	XmppJid string `json: "xmpp_jid"`
+	XmppJid string `json:"xmpp_jid"`
 
 	// Whether the user has been deleted or not.
-	IsDeleted bool `json: "is_deleted"`
+	IsDeleted bool `json:"is_deleted"`
 
 	// User's full name.
-	Name string `json: "name"`
+	Name string `json:"name"`
 
 	// The date in ISO-8601 when the user was last active.
-	LastActive string `json: "last_active"`
+	LastActive string `json:"last_active"`
 
 	// User's title.
-	Title string `json: "title"`
+	Title string `json:"title"`
 
 	// The date in ISO-8601 when the user was created.
-	Created string `json: "created"`
+	Created string `json:"created"`
 
 	// User's ID
-	Id int `json: "id"`
+	Id int `json:"id"`
 
 	// User's @mention name.
-	MentionName string `json: "mention_name"`
+	MentionName string `json:"mention_name"`
 
 	// Whether or not this user is an admin of the group.
-	IsGroupAdmin bool `json: "is_group_admin"`
+	IsGroupAdmin bool `json:"is_group_admin"`
 
 	// The desired user timezone.
-	Timezone string `json: "timezone"`
+	Timezone string `json:"timezone"`
 
 	// Whether or not this user is a guest or registered user.
-	IsGuest bool `json: "is_guest"`
+	IsGuest bool `json:"is_guest"`
 
 	// User's email
-	Email string `json: "email"`
+	Email string `json:"email"`
 
 	// URL to user's photo. 125px on the longest side.
-	PhotoUrl string `json: "photo_url"`
+	PhotoUrl string `json:"photo_url"`
 }
 
 
